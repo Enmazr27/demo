973028e73495c3441dc721c4d9f59b5b19570f4e
Enmazr27 <vinhnq27122003@gmail.com>
2024-08-26 16:44:25 +0700

    hi

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/public/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/oauth2/authorization/keycloak")
+                ).logout(logout ->
+                        logout.logoutUrl("/oauth2/logout")
+                );
+
+        return http.build();
+    }
+
+}
